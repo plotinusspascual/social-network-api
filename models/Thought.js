49a9f562bac16072f118637eee3338b785e13909
plotinusspascual <plotinusspascual@gmail.com>
2022-02-25 20:45:38 -0800

    added Reaction schema as a subdocument schema for Thought model

---
+++ models/Thought.js
@@ -0,0 +1,63 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: [{
+      type: String,
+      required: true,
+    }],
+    reactions: [ReactionSchema]
+  },
+    {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    }
+  },
+    {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+})
+
+const Thought = model('Thought', ThoughtSchema);
+module.exports = Thought;
