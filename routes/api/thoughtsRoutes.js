1b2007f6ad179c5596b1bbb33a2adc6c94e1aaa9
plotinusspascual <plotinusspascual@gmail.com>
2022-02-25 22:34:11 -0800

    created thoughtRoute api route

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getAllThought,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughts-controller');
+
+router.route('/').get(getAllThought).post(addThought);
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:id/reactions').post(addReaction);
+
+router.route('/:id/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
